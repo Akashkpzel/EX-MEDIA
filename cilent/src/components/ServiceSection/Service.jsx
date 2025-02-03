49096145466a77bafac7167848195cfa1d86c758
AKASH K P <akashreeba@gmail.com>
2025-02-03 10:15:26 +0530

    update services

--- cilent/src/components/ServiceSection/Service.jsx
+++ cilent/src/components/ServiceSection/Service.jsx
@@ -3,41 +3,41 @@ import './Service.css'
 const Service = () => {
     return (
         <>
-        
-        <section className='section-service'>
-            <div className='service-title'>
-                <h1>How We Help You For Brand Positioning & Empower the Business</h1>
-                <p>Unlock untapped potential and accelerate business with tailored strategies, Dominate the market and leave competitors in the dust</p>
-            </div>
 
-            <div className='services'>
-                <div className='service'>
-                    <i className="fa-solid fa-chart-simple fa-xl icon"></i>
-                    <h3>Strategies</h3>
-                    <p>Shaping brands that resonate and endure. We craft strategic identities with lasting impact.</p>
+            <section className='section-service'>
+                <div className='service-title'>
+                    <h1>How We Help You For Brand Positioning & Empower the Business</h1>
+                    <p>Unlock untapped potential and accelerate business with tailored strategies, Dominate the market and leave competitors in the dust</p>
                 </div>
-                <div className='service'>
-                    <i className="fa-solid fa-chart-simple fa-xl icon"></i>
-                    <h3>Strategies</h3>
-                    <p>Shaping brands that resonate and endure. We craft strategic identities with lasting impact.</p>
-                </div>
-                <div className='service'>
-                    <i className="fa-solid fa-chart-simple fa-xl icon"></i>
-                    <h3>Strategies</h3>
-                    <p>Shaping brands that resonate and endure. We craft strategic identities with lasting impact.</p>
-                </div>
-                <div className='service'>
-                    <i className="fa-solid fa-chart-simple fa-xl icon"></i>
-                    <h3>Strategies</h3>
-                    <p>Shaping brands that resonate and endure. We craft strategic identities with lasting impact.</p>
-                </div>
-            </div>
 
-            <button className='service-btn'>Get in touch</button>
+                <div className='services'>
+                    <div className='service'>
+                        <i className="fa-solid fa-chart-simple fa-xl icon"></i>
+                        <h3>Strategies</h3>
+                        <p>Shaping brands that resonate and endure. We craft strategic identities with lasting impact.</p>
+                    </div>
+                    <div className='service'>
+                        <i className="fa-solid fa-photo-film fa-xl icon "></i>
+                        <h3>Media</h3>
+                        <p>We craft captivating visuals in photography and videography, transforming ideas into unforgettable moments with excellence.</p>
+                    </div>
+                    <div className='service'>
+                        <i className="fa-solid fa-microchip fa-xl icon"></i>
+                        <h3>AI</h3>
+                        <p>Elevate advertising with AI-powered apps—smarter campaigns, personalized content, and data-driven results.</p>
+                    </div>
+                    <div className='service'>
+                        <i className="fa-solid fa-chart-simple fa-xl icon"></i>
+                        <h3>Strategies</h3>
+                        <p>Shaping brands that resonate and endure. We craft strategic identities with lasting impact.</p>
+                    </div>
+                </div>
 
-        </section>
+                <button className='service-btn'>Get in touch</button>
 
-        
+            </section>
+
+
         </>
     )
 }
